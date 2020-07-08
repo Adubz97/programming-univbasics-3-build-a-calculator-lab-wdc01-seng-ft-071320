d41f9171cb89505163353ffb027424bfcb75d13b
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-07-08 18:28:16 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,27 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
 def addition(num1, num2)
   sum = num1 + num2
-  p sum
+  sum
 end
 
 def subtraction(num1, num2)
   difference = num1 - num2 
-  p difference
+  difference
 end
 
 def division(num1, num2)
   dividing = num1 / num2
-  p dividing
+  dividing
 end
 
 def multiplication(num1, num2)
   multiply =  num1 * num2
-  p multiply
+  multiply
 end
 
 def modulo(num1, num2)
    modulo = num1 % num2
+   modulo
 end
 
 def square_root(num)
-  p Math.sqrt(num)
+  Math.sqrt(num)
 end
